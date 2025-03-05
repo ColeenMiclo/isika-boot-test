50f7c1a359b3c075dcec8dd492384dc28b3aaba5
Coleen Miclo <coleen.miclo@gmail.com>
2025-03-05 08:28:46 +0100

    new version

--- src/main/java/com/formation/controller/TutorialController.java
+++ src/main/java/com/formation/controller/TutorialController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.formation.model.Tutorial;
 import com.formation.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:3306")
+@CrossOrigin(origins = "http://13.60.28.212:8080")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
